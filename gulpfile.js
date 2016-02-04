0ea606fbe6d495eff0728f9f2c7eb6a4ff5d0900
Benjamin Auzanneau (necraidan) <benjamin.auzanneau.tm@gmail.com>
2016-02-04 10:53:46 +0100

    Ajout de tâches

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,33 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 var size = require('gulp-filesize');
 var yarg = require('yargs').argv;
+var uglify = require('gulp-uglify');
+
+var TEAM_NAME = 'TheCodeAwakens';
+var DEV_NAME = 'BenjaminAuzanneau';
 
 gulp.task('zip', function(){
-  return gulp.src('src/*')
-        .pipe(zip('archive.zip'))
-        .pipe(gulp.dest('dist'));
+  if(yarg.ex){
+    var zipName = TEAM_NAME+'_'+DEV_NAME+'_'+yarg.ex+'.zip';
+    return gulp.src([yarg.ex+'/*', '!'+yarg.ex+'/*.zip'])
+          .pipe(zip(zipName))
+          .pipe(gulp.dest(yarg.ex));
+  }else{
+    console.log('ERROR NOT --exercice argument');
+  }
+});
+
+gulp.task('uglify', function(){
+  return gulp.src(yarg.ex+'/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest(yarg.ex));
 });
 
 gulp.task('size', function(){
   return gulp.src('dist/archive.zip').pipe(size());
 });
+
+gulp.task('final', ['zip']);
 
 // Default Task
 gulp.task('default', ['zip']);
